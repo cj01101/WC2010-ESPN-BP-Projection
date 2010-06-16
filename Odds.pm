5a77a81e577f90a6bfe81408cea62caad8628733
CJ <cj01101@gmail.com>
2010-06-15 20:42:55 -0700

    import the code

---
+++ Odds.pm
@@ -0,0 +1,169 @@
+package Odds;
+
+sub get_odds
+{
+    return {
+        A => {
+            win => {
+                France         => '11/8',
+                Uruguay        => '11/4',
+                Mexico         => '7/2',
+                'South Africa' => '13/2',
+            },
+            place => {
+                France         => '12/5',
+                Uruguay        => '12/5',
+                Mexico         => '13/5',
+                'South Africa' => '3',
+            },
+            qualify => {
+                France         => '1/2',
+                Uruguay        => '7/8',
+                Mexico         => '11/10',
+                'South Africa' => '9/5',
+            },
+        },
+        B => {
+            win => {
+                Argentina     => '2/9',
+                'South Korea' => '5',
+                Nigeria       => '40',
+                Greece        => '200',
+            },
+            place => {
+                Argentina     => '4',
+                'South Korea' => '6/4',
+                Nigeria       => '6/4',
+                Greece        => '10',
+            },
+            qualify => {
+                Argentina     => '1/33',
+                'South Korea' => '4/5',
+                Nigeria       => '7/5',
+                Greece        => '14',
+            },
+        },
+        C => {
+            win => {
+                England  => '4/7',
+                USA      => '16/5',
+                Slovenia => '6',
+                Algeria  => '100',
+            },
+            place => {
+                England  => '9/4',
+                USA      => '6/5',
+                Slovenia => '9/4',
+                Algeria  => '25',
+            },
+            qualify => {
+                England  => '1/10',
+                USA      => '7/10',
+                Slovenia => '7/4',
+                Algeria  => '20',
+            },
+        },
+        D => {
+            win => {
+                Germany   => '1/4',
+                Ghana     => '15/4',
+                Serbia    => '33',
+                Australia => '150',
+            },
+            place => {
+                Germany   => '11/4',
+                Ghana     => '13/5',
+                Serbia    => '15/8',
+                Australia => '4',
+            },
+            qualify => {
+                Germany   => '1/5',
+                Ghana     => '7/5',
+                Serbia    => '4',
+                Australia => '7',
+            },
+        },
+        E => {
+            win => {
+                Netherlands => '1/9',
+                Cameroon    => '40',
+                Japan       => '9',
+                Denmark     => '25',
+            },
+            place => {
+                Netherlands => '13/2',
+                Cameroon    => '19/4',
+                Japan       => '6/4',
+                Denmark     => '31/20',
+            },
+            qualify => {
+                Netherlands => '1/7',
+                Cameroon    => '5',
+                Japan       => '10/3',
+                Denmark     => '5/2',
+            },
+        },
+        F => {
+            win => {
+                Italy         => '4/6',
+                Paraguay      => '21/10',
+                Slovakia      => '11',
+                'New Zealand' => '66',
+            },
+            place => {
+                Italy         => '5/2',
+                Paraguay      => '13/10',
+                Slovakia      => '21/10',
+                'New Zealand' => '20',
+            },
+            qualify => {
+                Italy         => '1/9',
+                Paraguay      => '3/10',
+                Slovakia      => '11/4',
+                'New Zealand' => '16',
+            },
+        },
+        G => {
+            win => {
+                Brazil        => '4/11',
+                Portugal      => '4',
+                'Ivory Coast' => '11/2',
+                'North Korea' => '200',
+            },
+            place => {
+                Brazil        => '12/5',
+                Portugal      => '5/4',
+                'Ivory Coast' => '2',
+                'North Korea' => '28',
+            },
+            qualify => {
+                Brazil        => '1/9',
+                Portugal      => '6/5',
+                'Ivory Coast' => '8/11',
+                'North Korea' => '25',
+            },
+        },
+        H => {
+            win => {
+                Spain       => '1/4',
+                Chile       => '6',
+                Switzerland => '14',
+                Honduras    => '66',
+            },
+            place => {
+                Spain       => '15/4',
+                Chile       => '23/20',
+                Switzerland => '9/4',
+                Honduras    => '7',
+            },
+            qualify => {
+                Spain       => '1/14',
+                Chile       => '4/6',
+                Switzerland => '13/8',
+                Honduras    => '6',
+            },
+        },
+    };
+}
+
+1;
---
+++ group_projection.pl
@@ -0,0 +1,69 @@
+#!/usr/local/bin/perl
+
+use Odds;
+use Picks;
+
+# odds from http://www.oddschecker.com/football/internationals/world-cup
+
+my $odds = Odds->get_odds();
+my $picks = Picks->get_picks();
+
+# calculate each user's expected number of points at the end of the group stages
+my $expectation = {}; # $user => $expectation
+foreach my $user ( keys %{ $picks } ) {
+    foreach my $group ( keys %{ $picks->{ $user }{ group_picks } } ) {
+
+        my $win   = $picks->{ $user }{ group_picks }{ $group }[0];
+        my $place = $picks->{ $user }{ group_picks }{ $group }[1];
+
+        # expectation for 1 to qualify (1 point)
+        $expectation->{ $user } += get_percentage( $odds->{ $group }{ qualify }{ $win } );
+
+        # expectation for 2 to qualify (1 point)
+        $expectation->{ $user } += get_percentage( $odds->{ $group }{ qualify }{ $place } );
+
+        # expectation for 1 to win (1 point bonus for getting the rank correct)
+        $expectation->{ $user } += get_percentage( $odds->{ $group }{ win }{ $win } );
+
+        # expectation for 2 to place (1 point bonus for getting the rank correct)
+        $expectation->{ $user } += get_percentage( $odds->{ $group }{ place }{ $place } );
+    }
+}
+
+# output in order of expectation
+foreach my $user ( get_ranked_users( $expectation ) ) {
+    my $paid = ( $picks->{ $user }{ paid } ) ? ' ' . '$' : ''; # this user put money toward the pot
+    print $user
+        . "\t" . sprintf( "%0.2f", $expectation->{ $user } ) . $paid
+        . "\t(" . $picks->{ $user }{ champion } . ')' # show who this user picked to win it all, since that will probably determine our winner
+        . "\n";
+}
+
+
+
+sub get_percentage # translate 2/1 => 0.33
+{
+    my $odds = shift;
+
+    if ( $odds =~ /\// ) {
+        my ( $numerator, $denominator ) = split( /\//, $odds );
+        return 1 / ( 1 + ( $numerator / $denominator ) );
+    } else {
+        return 1 / ( 1 + int( $odds ) );
+    }
+}
+
+sub get_ranked_users
+{
+    my $expectation = shift;
+
+    my @ranked;
+    my %saw = undef;
+    my @unique_expectation_values = grep( ! $saw{ $_ } ++, values %{ $expectation } ); # unique expectation values
+    foreach my $value ( sort { $b <=> $a } @unique_expectation_values ) {
+        foreach my $user ( keys %{ $expectation } ) {
+            push( @ranked, $user ) if ( $expectation->{ $user } == $value ); # build @ranked user list
+        }
+    }
+    return @ranked;
+}
